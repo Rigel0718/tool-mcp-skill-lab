4c67191e12ac443d204a2195a600616fe0c25242
geonhee <pollux0718@gmail.com>
2026-09-14 23:06:22 +0900

    feat: implement tool_registry

---
+++ src/tool_registry.py
@@ -0,0 +1,9 @@
+from tools import read_file, write_file, list_files, run_command
+
+
+TOOL_REGISTRY = {
+    "list_files": list_files,
+    "read_file": read_file,
+    "write_file": write_file,
+    "run_command": run_command, 
+}
